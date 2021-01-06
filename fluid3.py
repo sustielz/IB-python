1319529736e18377ad0caaa059330c9c07ebdbe1
Jackie Sustiel <43007813+sustielz@users.noreply.github.com>
2021-01-06 17:36:52 -0500

    Add files via upload

---
+++ fluid3.py
@@ -0,0 +1,171 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+
+#### 3D Fluid solver using fft
+class FLUID(object):
+  
+  #### Fluid domain properties ####    
+    @property  # Box size
+    def L(self): return self._L
+    
+    @L.setter
+    def L(self, L):
+        self._L = float(L)
+        self._h = self.L/self.N
+
+    @property  # Grid spacing
+    def h(self): return self._h
+            
+    def __init__(self, N=64, L=1., rho=1., mu=.01, dt=.01):
+        self.N = N 
+        self.L = L 
+        self.ip = (np.arange(N)+1)%N   # Grid index shifted left
+        self.im = np.arange(N)-1   # Grid index shifted right
+                            
+        self.u = np.zeros([3,N,N,N]) #Fluid velocity
+        self.rho = rho  # Fluid density
+        self.mu = mu    # viscosity 
+        
+        self.t = 0      # Time
+        self.dt = dt    # Time step        
+        
+        self.init_a()   # Matrix for use in fluid solver
+    
+    def boundary(self, u): return u  # Override to impose boundary conditions on the fluid
+    
+    def step_u(self, ff):
+        self.u, uu = self.fluid(self.u, ff)
+        return uu
+        
+    def init_a(self):
+        N = self.N
+        a = np.zeros([3,3,N,N,N])
+        a[0, 0] = 1
+        a[1, 1] = 1
+        a[2, 2] = 1
+
+        for m1 in range(N):
+            for m2 in range(N):
+                for m3 in range(N):
+                    if not ( (m1==0 or (N%2==0 and m1==int(N/2))) 
+                            and (m2==0 or (N%2==0 and m2==int(N/2)))
+                            and (m3==0 or (N%2==0 and m3==int(N/2)))):
+                        t=(2*np.pi/N)*np.array([m1, m2, m3]);
+                        s=np.sin(t);
+
+                        #### Note matrix multiplication might matter here
+                        ss=np.outer(s, s)/np.inner(s, s)
+
+                        #     a(m1+1,m2+1,:,:)=a(m1+1,m2+1,:,:)-(s*s')/(s'*s)
+                        a[0,0,m1,m2,m3]-=ss[0,0]
+                        a[0,1,m1,m2,m3]-=ss[0,1]
+                        a[0,2,m1,m2,m3]-=ss[0,2]
+                        
+                        a[1,0,m1,m2,m3]-=ss[1,0]
+                        a[1,1,m1,m2,m3]-=ss[1,1]
+                        a[1,2,m1,m2,m3]-=ss[1,2]
+                        
+                        a[2,0,m1,m2,m3]-=ss[2,0]
+                        a[2,1,m1,m2,m3]-=ss[2,1]
+                        a[2,2,m1,m2,m3]-=ss[2,2]
+
+        for m1 in range(N):
+            for m2 in range(N):
+                for m3 in range(N):
+                    t=(np.pi/N)*np.array([m1, m2, m3]);
+                    s=np.sin(t);
+                    a[:,:,m1,m2,m3] /= (1+(self.dt/2)*(self.mu/self.rho)*(4/(self.h**2))*(np.inner(s, s)));
+        self.a = a
+    
+    # Second order centered Laplacian
+    def laplacian(self, u): 
+        im, ip, h = self.ip, self.im, self.h
+        w=(u[:,ip,:,:]+u[:,im,:,:]+u[:,:,ip,:]+u[:,:,im,:]+u[:,:,:,ip]+u[:,:,:,im]-6*u)/(h**2);
+        return w
+    
+    
+    
+    def skew(self, u):
+        w=u*1. #note that this is done only to make w the same size as u
+        w[0]=self.sk(u,u[0]);
+        w[1]=self.sk(u,u[1]);
+        w[2]=self.sk(u,u[2]);
+        return w
+    
+    def sk(self, u, g):
+        ip, im, h = self.ip, self.im, self.h
+        ii = np.arange(self.N)
+        return ((u[0][ip,:,:]+u[0])*g[ip,:,:]
+                -(u[0][im,:,:]+u[0])*g[im,:,:]
+                +(u[1][:,ip,:]+u[1])*g[:,ip,:]
+                -(u[1][:,im,:]+u[1])*g[:,im,:]
+                +(u[2][:,:,ip]+u[1])*g[:,:,ip]
+                -(u[2][:,:,im]+u[1])*g[:,:,im])/(6*h);
+
+    
+    # Time step the fluid
+    def fluid(self, u, ff): 
+        self.boundary(u)
+        uu = np.zeros(np.shape(u), dtype=np.complex)
+        uuu = np.zeros(np.shape(u), dtype=np.complex)
+        
+        a, dt, rho, mu = self.a, self.dt, self.rho, self.mu
+        w=u-(dt/2)*self.skew(u)+(dt/(2*rho))*ff; # Get RHS
+        w=np.fft.fft(w, axis=1)
+        w=np.fft.fft(w, axis=2)
+        w=np.fft.fft(w, axis=3)
+        uu[0]=a[0,0]*w[0]+a[0,1]*w[1]+a[0,2]*w[2] # Solve for LHS
+        uu[1]=a[1,0]*w[0]+a[1,1]*w[1]+a[1,2]*w[2] 
+        uu[2]=a[2,0]*w[0]+a[2,1]*w[1]+a[2,2]*w[2] 
+        uu=np.fft.ifft(uu,axis=3)
+        uu=np.fft.ifft(uu,axis=2)
+        uu=np.fft.ifft(uu,axis=1).real #Get u at midpoint in time
+        self.boundary(uu)
+        
+        w=u-dt*self.skew(uu)+(dt/rho)*ff+(dt/2)*(mu/rho)*self.laplacian(u)# Get RHS
+        w=np.fft.fft(w, axis=1)
+        w=np.fft.fft(w, axis=2)
+        w=np.fft.fft(w, axis=3)
+        uuu[0]=a[0,0]*w[0]+a[0,1]*w[1]+a[0,2]*w[2]# Solve for LHS
+        uuu[1]=a[1,0]*w[0]+a[1,1]*w[1]+a[1,2]*w[2]
+        uuu[2]=a[2,0]*w[0]+a[2,1]*w[1]+a[2,2]*w[2]
+       
+        uuu=np.fft.ifft(uuu,axis=3)
+        uuu=np.fft.ifft(uuu,axis=2)
+        uuu=np.fft.ifft(uuu,axis=1).real # Get u at next timestep
+        return uuu, uu
+   
+
+  #### Methods for plotting and visualization
+    
+#     @property
+#     def vorticity(self):
+#         u, h, ip, im = self.u, self.h, self.ip, self.im
+#         ii = im+1
+# #         return (u[1,ip,:]-u[1,im,:]-u[0,:,ip]+u[0,:,im])/(2*h);
+#         vorticity=(u[1][np.meshgrid(ip, ii)]
+#                        -u[1][np.meshgrid(im,ii)]
+#                        -u[0][np.meshgrid(ii,ip)]
+#                        +u[0][np.meshgrid(ii,im)])/(2*self.h);
+#         return vorticity
+
+#     def show_vorticity(self):
+#         vorticity=self.vorticity
+#         dvorticity=(np.max(vorticity)-np.min(vorticity))/5;
+#         dvorticity = max(dvorticity, 0.1)  ## Catch error on 0 (or uniform) vorticity
+#         return plt.imshow(vorticity,  vmin=-2*dvorticity, vmax=2*dvorticity, origin='lower', extent=[0, self.L, 0, self.L])
+# #         plt.colorbar()
+        
+#     def show_streamlines(self, ax, cmap=None,):
+#         X, Y = np.meshgrid(self.h*np.arange(self.N), self.h*np.arange(self.N))
+#         if cmap is None:
+#             stream = ax.streamplot(X, Y, self.u[0], self.u[1], color='black')
+#             return stream.lines, stream.arrows
+#         else:
+#             uu = np.sqrt(np.sum(self.u**2, axis=0))
+#             return plt.streamplot(X, Y, self.u[0], self.u[1], color=uu.transpose(), cmap=cmap)
+# #             plt.colorbar()
+            
+
+        
+        
---
+++ ib3.py
@@ -0,0 +1,149 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+import matplotlib.tri as tri
+
+#### Generic class for an immersed boundary in a 2D fluid
+class IB3(object):
+
+    def __init__(self, X, N, h, dt, K=1., v=None):
+        self.X = X     # Positions of boundary
+        self.N = N     # Fluid domain properties                
+        self.h = h
+        self.dt = dt
+        self.K = K      # Elastic stiffness
+    
+        self.Nb = np.shape(X)[1]             # number of boundary points        
+        self.dtheta = 2*np.pi/self.Nb        # spacing of boundary points
+        self.kp = (np.arange(self.Nb)+1)%self.Nb       # IB index shifted left
+        self.km = np.arange(self.Nb)-1       # IB index shifted right
+    
+    def step_XX(self, u): self.XX=self.X+0.5*self.dt*self.interp(u,self.X) # Euler step to midpoint
+       
+    def step_X(self, uu): self.X+=self.dt*self.interp(uu,self.XX) # full step using midpoint velocity            
+    
+    @property
+    def ff(self): return self.vec_spread(self.Forcesurf(self.XX, self.v) ,self.XX) # Force at midpoint
+    
+    def phi(self, r):     ## Discrete dirac delta function
+        w = np.zeros(4)
+        q=np.sqrt(1+4*r*(1-r))
+        w[3]=(1+2*r-q)/8
+        w[2]=(1+2*r+q)/8
+        w[1]=(3-2*r+q)/8
+        w[0]=(3-2*r-q)/8
+        return w
+
+    def interp(self, u, X):      ## Interpolate boundary velocity U from fluid velocity u
+        N, Nb, h = self.N, self.Nb, self.h
+        W = np.zeros([N, N, N])
+
+        U=np.zeros([3,Nb])
+        s=X/float(h)
+        i=np.array(np.floor(s), dtype=int)
+        r=s-i
+        for k in range(Nb):
+#             w = self.phi(r[2, k]).outer(self.phi(r[1, k]).outer(self.phi(r[0, k])))
+#             w = np.outer(self.phi(r[2, k]), np.outer(self.phi(r[1, k]), self.phi(r[0, k])))
+            w = self.phi(r[0, k])[:, None, None]*self.phi(r[1, k])[None, :, None]*self.phi(r[2, k])[None, None, :]
+#             w = self.phi(r[2, k])[None, None, :]*self.phi(r[1, k])[None, :, None]*self.phi(r[0, k])[:, None, None]
+#             w = np.einsum('i,j,k',self.phi(r[2, k]), self.phi(r[1, k]), self.phi(r[0, k]))
+#             if k==0:
+#                 print('w 0:')
+#                 print(w)
+            i1 = np.arange(i[0,k]-1, i[0,k]+3)%N
+            i2 = np.arange(i[1,k]-1, i[1,k]+3)%N
+            i3 = np.arange(i[2,k]-1, i[2,k]+3)%N
+            iii = np.meshgrid(i1, i2, i3, indexing='ij')
+#             if k==0:
+#                 print('ii_interp')
+#                 print(ii)
+
+            U[0,k]=np.sum(w*u[0][iii]);
+            U[1,k]=np.sum(w*u[1][iii]);
+            U[2,k]=np.sum(w*u[2][iii]);
+#             W[ii] += w
+
+#         print('after interp:')
+#         plt.imshow(np.transpose(W))
+#         plt.colorbar()
+#         plt.scatter(X[0]/h, X[1]/h)
+#         plt.show()
+#         print(np.mean(abs(U)))
+        return U
+    
+    def vec_spread(self, F, X):   ## Spread boundary force F onto fluid domain ff
+        N, Nb, h = self.N, self.Nb, self.h
+        W = np.zeros([N, N, N])
+        
+        c=self.dtheta/h**3;
+        f=np.zeros([3,N,N,N]);
+        s=X/float(h)
+        i=np.array(np.floor(s), dtype=int)
+        r=s-i
+        for k in range(Nb):
+            w = self.phi(r[2, k])[None, None, :]*self.phi(r[1, k])[None, :, None]*self.phi(r[0, k])[:, None, None]
+            i1 = np.arange(i[0,k]-1, i[0,k]+3)%N
+            i2 = np.arange(i[1,k]-1, i[1,k]+3)%N
+            i3 = np.arange(i[2,k]-1, i[2,k]+3)%N
+            iii = np.meshgrid(i1, i2, i3, indexing='ij')
+
+            f[0][iii]+=(c*F[0,k])*w #Spread force to fluid
+            f[1][iii]+=(c*F[1,k])*w
+            f[2][iii]+=(c*F[2,k])*w
+                       
+        return f 
+
+#   #### Methods for visualization/plotting
+#     def show_X(self, X=None, L=None):
+#         X = X or self.X
+#         L = L or self.N*self.h
+#         plt.scatter(X[0],X[1])
+#         plt.xlim([0,L])
+#         plt.ylim([0,L])    
+        
+    def show_phi(self, X=None, show_X=True):
+        N, Nb, h = self.N, self.Nb, self.h
+        W = np.zeros([N, N, N])
+
+        X = X or self.X
+        s=X/float(h)
+        i=np.array(np.floor(s), dtype=int)
+        r=s-i
+        for k in range(Nb):
+            w = self.phi(r[0, k])[:, None, None]*self.phi(r[1, k])[None, :, None]*self.phi(r[2, k])[None, None, :]
+
+            i1 = np.arange(i[0,k]-1, i[0,k]+3)%N
+            i2 = np.arange(i[1,k]-1, i[1,k]+3)%N
+            i3 = np.arange(i[2,k]-1, i[2,k]+3)%N
+            iii = np.meshgrid(i1, i2, i3, indexing='ij')
+
+            W[iii] += w
+        return W
+            
+            
+        
+            
+    def grad(self,A,B,C):
+        base=np.sqrt(np.sum((B-C)**2));
+        n=np.cross(B-A,C-A);
+        un=n/np.linalg.norm(n, axis=0);
+        dct=np.cross(B-C,un);
+        dct=dct/np.linalg.norm(dct, axis=0)
+        return dct*base/2
+        
+    
+    def Forcesurf(self, X, v): #thi force calculation uses the surface energy(the area) as its energy function.
+        Nb = self.Nb
+        K = self.K
+        F= np.zeros([3, Nb])
+        
+        numtri=np.shape(v)[0]
+        for ti in range(numtri):
+            A=X[:,v[ti,0]]
+            B=X[:,v[ti,1]]
+            C=X[:,v[ti,2]]
+            F[:,v[ti,0]]+=self.grad(A,B,C);
+            F[:,v[ti,1]]+=self.grad(B,A,C);
+            F[:,v[ti,2]]+=self.grad(C,A,B);
+       
+        return -F*K;
